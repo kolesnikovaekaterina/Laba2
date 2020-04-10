f79638167f6f79878f704e6a4382deae05aa5656
kolesnikovaekaterina <62741892+kolesnikovaekaterina@users.noreply.github.com>
2020-04-10 04:02:35 +0400

    Add files via upload

---
+++ Product.h
@@ -0,0 +1,31 @@
+#ifndef PRODUCT_H_INCLUDED
+#define PRODUCT_H_INCLUDED
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Product
+{
+private:
+string name;
+float price;
+int weight;
+
+public:
+Product ();
+
+bool operator> (const Product &qq) const;
+bool operator< (const Product &qq) const;
+bool operator>= (const Product &qq) const;
+bool operator<= (const Product &qq) const;
+bool operator== (const Product &qq) const;
+bool operator!= (const Product &qq) const;
+
+friend ostream& operator << (ostream &out, const Product &qq);
+friend istream& operator >> (istream &in, Product &qq);
+Product& operator=(Product &qq);
+
+};
+
+#endif // PRODUCT_H_INCLUDED
